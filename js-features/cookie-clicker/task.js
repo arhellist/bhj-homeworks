10eb35ae34439fff34ab8a2f003e515179de44e1
Aleksey Pasechnik <Arhellist@mail.ru>
2022-11-21 23:23:41 +0400

    Домашнее задание Печеньки с 2-мя звездами оба

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,6 +2,7 @@ let clickCount = Number(document.getElementById('clicker__counter').textContent)
 let image = document.getElementById('cookie');
 let clean = document.getElementById('clear');
 let speed = Number(document.getElementById('clicker__speed').textContent);
+let best = Number(document.getElementById('clicker__best').textContent);
 let now = new Date();
 
 
@@ -11,6 +12,10 @@ image.onclick = function() {
     let clickTime = timeOnClick - now;
     speed = 1000 / clickTime;
     document.getElementById('clicker__speed').textContent = speed.toFixed(2);
+    if (speed.toFixed(2) > best) { //быстрее чем 9.71 не получается, возможно из за скорости выполнения кода
+        best = speed.toFixed(2);
+        document.getElementById('clicker__best').textContent = speed.toFixed(2);
+    }
     clickCount = clickCount + 1;
     document.getElementById('clicker__counter').textContent = clickCount;
     image.setAttribute('width', '250');
@@ -24,4 +29,6 @@ clean.onclick = function() {
     clickCount = 0;
     document.getElementById('clicker__counter').textContent = 0;
     document.getElementById('clicker__speed').textContent = 0;
+    best = 0;
+    document.getElementById('clicker__best').textContent = 0;
 }
