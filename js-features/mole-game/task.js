686936eca8f596e50153c3e178d085063fb2f1c9
Aleksey Pasechnik <Arhellist@mail.ru>
2022-11-24 21:35:53 +0400

    Домашнее задание Печеньки с 2-мя звездами оба

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,11 @@
+let dead = document.getElementById('dead');
+let lost = document.getElementById('lost');
+
+
+for (let index = 0; 9; index++) {
+    getHole = index => document.getElementById(`hole${index}`)
+    if (getHole.classList.includes('hole_has-mole')) {
+
+    }
+
+}
