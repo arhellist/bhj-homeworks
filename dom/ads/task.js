1a96bb61c7b2e6bd1e0a254c1781ad1e86f1341e
Aleksey Pasechnik <Arhellist@mail.ru>
2022-11-28 00:17:25 +0400

    Ротатор Рекламы

---
+++ dom/ads/task.js
@@ -0,0 +1,25 @@
+const rotatorCase = document.querySelectorAll('.rotator__case');
+let count = 0;
+let collor;
+let timeInterval = rotatorCase[count].getAttribute('data-speed');
+
+const timeCount = setInterval(() => {
+    count += 1;
+    if (count === rotatorCase.length) {
+        count = 0;
+    }
+
+    timeInterval = rotatorCase[count].getAttribute('data-speed');
+    setTimeout(() => {
+        console.log(timeInterval);
+    }), timeInterval;
+
+    rotatorCase[count].classList.add('rotator__case_active');
+    collor = rotatorCase[count].getAttribute('data-color');
+    rotatorCase[count].style.color = collor;
+    if ((count - 1) === -1) {
+        rotatorCase[rotatorCase.length - 1].classList.remove('rotator__case_active');
+    } else {
+        rotatorCase[count - 1].classList.remove('rotator__case_active');
+    }
+}, 1000)
