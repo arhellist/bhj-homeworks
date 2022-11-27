486a2bdda5587100449111d33ec28a231c2b51c8
Aleksey Pasechnik <Arhellist@mail.ru>
2022-11-27 17:28:31 +0400

    ДЗ выпадающие списки

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,12 +1,10 @@
 const dropdownValue = document.querySelector('.dropdown__value');
 const dropdownList = document.querySelector('.dropdown__list');
 const dropdownItem = document.querySelectorAll('.dropdown__item');
-const dropdownLink = document.querySelectorAll('.dropdown__link');
 
 dropdownValue.addEventListener('click', () => {
     dropdownList.classList.add('dropdown__list_active');
 });
-
 
 dropdownItem.forEach(el => {
     el.addEventListener('click', (event) => {
