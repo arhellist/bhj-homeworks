3d0c38476352758f95fe9d83a16124afc46b89dc
Aleksey Pasechnik <Arhellist@mail.ru>
2022-12-18 16:21:00 +0400

    ДЗ Лоадер

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,92 +1,48 @@
 let loader = document.querySelector('.loader');
-let rebit;
-//let url = 'https://students.netoservices.ru/nestjs-backend/slow-get-courses';
-
+let url = 'https://students.netoservices.ru/nestjs-backend/slow-get-courses';
 const items = document.querySelector('#items');
 
-loadCourse();
-loaded('https://students.netoservices.ru/nestjs-backend/slow-get-courses', getValue);
+
+xhrLoad();
 
 
-
-function loaded(url, callback) {
+function xhrLoad() {
     const xhr = new XMLHttpRequest();
-
     xhr.open('GET', url, true);
-
-    xhr.responseType = '';
-
     xhr.send();
+
+    xhr.addEventListener('readystatechange', () => {
+
+        if (xhr.readyState === xhr.DONE) {
+            loader.classList.remove('loader_active');
+        }
+    });
+
     xhr.onload = function() {
-        if (xhr.status != 200) {
-            alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
-        } else {
-            return callback(JSON.parse(xhr.response));
-        }
-    };
-}
-/*
-let xhr = new XMLHttpRequest();
-xhr.addEventListener('readystatechange', () => {
-    rebit = xhr.responseText;
-    console.log(rebit);
-    if (xhr.readyState === xhr.DONE) {
-        loader.classList.remove('loader_active');
-    }
-});
-xhr.open('GET', 'url', true);
-xhr.send();*/
+        //console.log(xhr.responseText);
+        const jsn = JSON.parse(xhr.responseText);
+        obj = new Object;
+        obj = jsn.response.Valute;
+        console.log(obj);
 
+        for (let element in obj) {
+            console.log(obj[element].CharCode, obj[element].Value);
 
+            let Item = document.createElement('div').classList.add('item');
+            let ItemCode = document.createElement('div').classList.add('item__code');
+            let ItemValue = document.createElement('div').classList.add('item__value');
+            let ItemCurency = document.createElement('div').classList.add('item__currency');
 
-function addElem(...data) {
-    const Item = document.createElement('div').classList.add('item');
-    const ItemCode = document.createElement('div').classList.add('item__code');
-    const ItemValue = document.createElement('div').classList.add('item__value');
-    const ItemCurency = document.createElement('div').classList.add('item__currency');
+            ItemCode.innerText = obj[element].CharCode;
+            ItemValue.innerText = obj[element].Value;
+            ItemCurency.innerText = 'руб.';
 
-    ItemCode.innerText = data[0];
-    ItemValue.innerText = data[1];
-    ItemCurency.innerText = 'руб.';
+            items.insertAdjacentElement('beforeend', 'Item');
+            Item.insertAdjacentElement('beforeend', 'ItemCode');
+            Item.insertAdjacentElement('beforeend', 'ItemValue');
+            Item.insertAdjacentElement('beforeend', 'ItemCurency');
 
-    items.insertAdjacentElement('beforeend', 'Item');
-    Item.insertAdjacentElement('beforeend', 'ItemCode');
-    Item.insertAdjacentElement('beforeend', 'ItemValue');
-    Item.insertAdjacentElement('beforeend', 'ItemCurency');
-}
-
-
-function getValue(jsn) {
-    const keysValuteArr = jsn.keys(jsn.response.Valute);
-    let currencies;
-    let course;
-    let arr = [];
-
-    for (let index of keysValuteArr) {
-        currencies = jsn.response.Valute[index].CharCode;
-        course = jsn.response.Valute[index].Value;
-        addElem(currencies, course);
-        arr.push({ currencies, course });
-    }
-    localStorage.removeItem('exchangeRate');
-    localStorage.setItem('exchangeRate', JSON.stringify(arr));
-
-}
-
-function loadCourse() {
-    let arr = JSON.parse(localStorage.getItem('exchangeRate'));
-    let currencies;
-    let course;
-    if (arr != null) {
-        for (let index of arr) {
-            currencies = index.currencies;
-            course = index.course;
-
-            addElement(currencies, course);
         }
     }
+
 }
-
-
-/*
- */
